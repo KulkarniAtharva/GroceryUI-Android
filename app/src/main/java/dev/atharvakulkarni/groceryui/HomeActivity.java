0e9ab79f0797ba27e806ad2c90a13a679ca8a45b
Atharva Kulkarni <atharvakulkarni2204@gmail.com>
2020-12-01 16:28:52 +0530

    home section done

--- app/src/main/java/dev/atharvakulkarni/groceryui/HomeActivity.java
+++ app/src/main/java/dev/atharvakulkarni/groceryui/HomeActivity.java
@@ -5,10 +5,11 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class HomeActivity extends AppCompatActivity {
-
+public class HomeActivity extends AppCompatActivity
+{
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
     }
